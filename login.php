aee366cd66d7dfaa66f38791f7ddc1591d3af485
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-07-16 01:15:35 -0700

    initial setup

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="I'm a Junior Front-End Web Developer. I'm an analytical problem solver with well-developed soft skills and a mentor to my peers.">
+    <meta name="keywords" content="Web Development, Front-End, Back-End">
+    <link rel="stylesheet" href="styles/normalize-fwd.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <h1>Login</h1>
+    </header>
+    <main>
+
+    
+
+    </main>
+</body>
+</html>
